fc7e422659e53c44e78bb364fa12bf9d0092be72
Bogdan Radoi <bogdanseb01@yahoo.com>
2022-01-07 11:11:38 +0000

    Libraries Included+ Window Created

---
+++ GraphicsProgramming/main.cpp
@@ -0,0 +1,69 @@
+#define GLEW_STATIC
+
+#include <glew.h>
+#include <SDL_opengl.h>
+#include<glm.hpp>
+#include<gtc/type_ptr.hpp>
+#include<SDL.h>
+#include<iostream>
+
+
+#undef main SDL_main
+using namespace std;
+
+
+
+
+
+
+
+int main()
+{
+	//Initialization
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,8 );
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,32 );
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1 );
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16 );
+
+	//Window Creation
+	SDL_Window* window = SDL_CreateWindow("My window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	SDL_GLContext GLContext = SDL_GL_CreateContext(window);
+
+	//Initializing GLEW
+	glewExperimental = GL_TRUE;
+	GLenum status = glewInit();
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	//Checking if GLEW is initialized correctly
+	if (status != GLEW_OK)
+	{
+		cout << "GLEW failed to initialize!" << endl;
+	}
+
+	//Window Loop
+	while (true)
+	{
+		glClearColor(0.0f, 0.15f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, 800, 600);
+
+		SDL_GL_SwapWindow(window);
+	}
+
+
+	//Clean up of all the objects
+	SDL_GL_DeleteContext(GLContext);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	SDL_Quit();
+
+
+
+	return 0;
+}
